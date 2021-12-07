7948596cfd1f51c57ec99738a5939642261a40fd
WenfishTseng <twy0171@outlook.com>
2021-12-07 10:17:19 +0800

    Update 2021-12-07T02:17:17.976Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -11,7 +11,8 @@ var allProducts = []; //dom
 
 var pdtList = document.querySelector(".pdtList");
 var categorySelect = document.querySelector(".categorySelect");
-var cartList = document.querySelector(".cartList"); // 監聽
+var cartList = document.querySelector(".cartList");
+var deleteBtn = document.querySelector(".deleteBtn"); // 監聽
 
 pdtList.addEventListener("click", function (e) {
   e.preventDefault;
@@ -24,15 +25,32 @@ categorySelect.addEventListener("change", function (e) {
 });
 cartList.addEventListener("click", function (e) {
   if (e.target.nodeName != "I") return;
-  console.log(e.target.dataset.cart);
   deleteCartId(e.target.dataset.cart);
+});
+deleteBtn.addEventListener("click", function (e) {
+  e.preventDefault;
+  deleteAllCarts();
 });
 initView();
 
 function initView() {
   getProductList();
   getCartList();
-} // 刪除特定產品
+} // 刪除購物車全部品項
+
+
+function deleteAllCarts() {
+  var url = "https://livejs-api.hexschool.io/api/livejs/v1/customer/".concat(api_path, "/carts");
+  axios["delete"](url).then(function (response) {
+    // 成功會回傳的內容
+    if (response.data.status) {
+      renderCartData(response.data.carts);
+    }
+  })["catch"](function (error) {
+    // 失敗會回傳的內容
+    console.log(error);
+  });
+} // 刪除購物車特定產品
 
 
 function deleteCartId(cartId) {
@@ -76,17 +94,23 @@ function getCartList() {
       renderCartData(response.data.carts);
     }
   })["catch"](function (error) {
-    console.log(error.response.data);
+    console.log(error);
   });
-} // 渲染購物車
+} // 渲染購物車商品列表
 
 
 function renderCartData(cartData) {
   var str = "";
+  var total = 0; // 購物車商品
+
   cartData.forEach(function (item) {
-    str += "          \n    <tr class=\"tableCart\">\n            <td scope=\"row\" class=\"d-flex align-items-center\">\n              <img\n                class=\"cartImg d-none d-md-block\"\n                src=\"".concat(item.product.images, "\"\n                alt=\"").concat(item.product.title, "\"\n              />\n              <h3 class=\"h4 d-inline-block ms-md-6 mb-0\">\n                ").concat(item.product.title, "\n              </h3>\n            </td>\n            <td>NT$").concat(item.product.price, "</td>\n            <td>").concat(item.quantity, "</td>\n            <td>NT$").concat(item.product.price, "</td>\n            <td>\n              <a class=\"#\" \n                ><i data-cart=\"").concat(item.id, "\" class=\"fas fa-times\" style=\"font-size: 24px\"></i>\n              </a>\n            </td>\n          </tr>");
+    str += "          \n    <tr class=\"tableCart\">\n            <td scope=\"row\" class=\"d-flex align-items-center\">\n              <img\n                class=\"cartImg d-none d-md-block\"\n                src=\"".concat(item.product.images, "\"\n                alt=\"").concat(item.product.title, "\"\n              />\n              <h3 class=\"h4 d-inline-block ms-md-6 mb-0\">\n                ").concat(item.product.title, "\n              </h3>\n            </td>\n            <td>NT$").concat(item.product.price, "</td>\n            <td>").concat(item.quantity, "</td>\n            <td>NT$").concat(item.product.price, "</td>\n            <td>\n              <a href=\"#\" class=\"btn btn-link\"\n                ><i data-cart=\"").concat(item.id, "\" class=\"fas fa-times\" style=\"font-size: 24px\"></i>\n              </a>\n            </td>\n          </tr>");
+    total += item.quantity * item.product.price;
   });
   cartList.innerHTML = str;
+  console.log("total", total);
+  var totalAmount = document.querySelector(".totalAmount");
+  totalAmount.innerHTML = "NT$ ".concat(total);
 } // 渲染Select : 種類
 
 
@@ -114,15 +138,17 @@ function renderSelectData(category) {
   }
 
   renderPdtData(filterAry);
-}
+} // 渲染產品清單
+
 
 function renderPdtData(data) {
   var str = "";
   data.forEach(function (item) {
-    str += "\n        <li class=\"col mt-0 mb-11\">\n      <div class=\"card h-100 border-0 position-relative\">\n        <span\n          class=\"\n            pdtBadge\n            badge\n            rounded-0\n            bg-primary\n            position-absolute\n            top-10\n            start-85\n            translate-middle\n          \"\n          >\u65B0\u54C1</span\n        >\n        <img\n          src=\"".concat(item.images, "\"\n          class=\"card-img-top\"\n          alt=\"").concat(item.title, "\"\n        />\n        <div class=\"card-body p-0\">\n          <a class=\"btn btn-primary w-100 rounded-0 fz-4 py-3 mb-3\" href=\"#\" data-num=\"").concat(item.id, "\"\n            >\u52A0\u5165\u8CFC\u7269\u8ECA</a\n          >\n          <h3 class=\"h4\" data-category=\"").concat(item.category, "\">").concat(item.title, "</h3>\n          <p class=\"h4 text-decoration-line-through mb-0\">").concat(item.origin_price, "</p>\n          <p class=\"h2 fw-bold mb-0\">").concat(item.price, "</p>\n        </div>\n      </div>\n    </li>\n    ");
+    str += "\n        <li class=\"col mt-0 mb-11\">\n      <div class=\"card h-100 border-0 position-relative\">\n        <span\n          class=\"\n            pdtBadge\n            badge\n            rounded-0\n            bg-primary\n            position-absolute\n            top-10\n            start-85\n            translate-middle\n          \"\n          >\u65B0\u54C1</span\n        >\n        <img\n          src=\"".concat(item.images, "\"\n          class=\"card-img-top\"\n          alt=\"").concat(item.title, "\"\n        />\n        <div class=\"card-body p-0\">\n          <a class=\"btn btn-primary w-100 rounded-0 fz-4 py-3 mb-3\" href=\"#\" data-num=\"").concat(item.id, "\"\n            >\u52A0\u5165\u8CFC\u7269\u8ECA</a\n          >\n          <h3 class=\"h4\" data-category=\"").concat(item.category, "\">").concat(item.title, "</h3>\n          <p class=\"h4 text-decoration-line-through mb-0\">NT$").concat(item.origin_price, "</p>\n          <p class=\"h2 fw-bold mb-0\">NT$").concat(item.price, "</p>\n        </div>\n      </div>\n    </li>\n    ");
   });
   pdtList.innerHTML = str;
-}
+} // 將單一商品加入購物車列表
+
 
 function addItemToCart(cartId) {
   var url = "https://livejs-api.hexschool.io/api/livejs/v1/customer/".concat(api_path, "/carts");
